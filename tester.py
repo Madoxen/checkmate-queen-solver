8cf6f6da82db6bc4a44d14c4f3454bf2019a33ef
MellerB <mellerbartomiej@gmail.com>
2020-11-16 18:53:42 +0100

    docs done

--- tester.py
+++ tester.py
@@ -1,16 +1,18 @@
 from chess import *
 
-q = "C5"
-
-k = "A7"
-
-K = "A8"
 
 b = Board()
+
+q = "C5"
+k = "A7"
+K = "A8"
+
 b.place_piece("q", q)
 b.place_piece("K", K)
 b.place_piece("k", k)
 
 print(b)
-
 print(b.get_mating_moves(q, K))
+
+
+
